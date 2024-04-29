2d626ef20988c0808b0fb436767266470f67e9ac
Geovanny Lara <jeffersonlara98@gmail.com>
2024-04-28 19:20:08 -0500

    First commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+    title: 'SEO Title',
+    description: 'SEO Title',
+};
+
+const page = () => {
+    return (
+        <div className="text-7xl">About Page</div>
+    )
+}
+
+export default page
